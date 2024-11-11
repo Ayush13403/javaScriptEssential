54479ef3811aa42566a9205f4c1105c2a9b2f04c
Ayush13403 <Ayush.kumar13403@gmail.com>
2024-11-11 11:29:35 -0500

    change

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,9 +25,16 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
+       
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+function deleteBook(index) {
+    books.splice(index, 1); // Remove the book at the given index
+    showbooks(); // Refresh the displayed list of books
 }
 
 function clearInputs() {
@@ -36,3 +43,8 @@ function clearInputs() {
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
 }
+
+function hyy(){
+    alert("hello");
+}
+
